581f254e2efe2e2260e5cd407884c4e7af5fb349
v1ncio <airfoil_elector0b@icloud.com>
2021-07-23 01:15:45 +0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Vincio on 7/23/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
